b68d8c9d825db85f59f6726e0b345da838a84125
amitdesh <amitdesh2014@gmail.com>
2020-07-19 11:33:26 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,36 @@
 class Manager
 
+    attr_accessor :name, :department, :age, :employee
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department 
+        @age = age
+        @employee = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(name, salary)
+        self.employee << Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        self.all.map {|manager| manager.department}
+    end
+
+    def self.average_age
+        agg_age = self.all.sum {|manager| manager.age}
+        no_managers = self.all.count
+        age = agg_age/no_managers
+        age  = age.to_f
+    end
+
+
+
 end
--- run.rb
+++ run.rb
@@ -3,6 +3,13 @@ require_relative "lib/Employee"
 require 'pry'
 
 
+#dan = Employee.new("Dan", 75000, "Bob")
+#mike = Employee.new("Mike", 76000, "Bob")
+chris = Manager.new("Chris", "Accting", 45)
+bob = Manager.new("Bob", "HR", 35)
+
+
+
 #Test your code here
 
 
