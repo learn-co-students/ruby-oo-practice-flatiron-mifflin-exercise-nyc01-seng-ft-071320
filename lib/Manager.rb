d86617ea3c6afcd9741f36199f0c89e45ce81aae
victorscholz <vgscholz@gmail.com>
2020-07-20 19:26:16 -0400

    working on it

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,40 @@
 class Manager
+    attr_accessor :name, :department, :age
+
+    @@all = []
+    @@all_departments = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+        @@all_departments << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def employees
+        Employee.map do |employees|
+            employees.manager == self
+        end
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        @@all_departments
+    end
+
+    def average_age
+        # age_sum = self.all.count do |age| 
+        #     age.manager == self 
+        # end
+        self.sum(0.0) / self.size
+    end
 
 end
